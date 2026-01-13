4e08ee3efaf017b30db26d923be8b8fe80b5efdf
Harry Solovay <harrysolovay@gmail.com>
2026-01-13 17:40:23 -0500

    update pipeline util

--- @crosshatch/util/embed.ts
+++ @crosshatch/util/embed.ts
@@ -2,7 +2,7 @@ import { type FeatureExtractionPipeline, pipeline } from "@huggingface/transform
 import { Cause, Effect } from "effect"
 
 let pipeline_: Promise<FeatureExtractionPipeline> | undefined
-const getPipeline = () => {
+export const getPipeline = () => {
   if (!pipeline_) {
     pipeline_ = pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", {
       dtype: "q8",
@@ -15,6 +15,7 @@ const getPipeline = () => {
 }
 
 export const embed = Effect.fn(function*(text: string): Effect.fn.Return<Array<number>, Cause.UnknownException> {
+  yield* Effect.tryPromise(() => navigator.storage?.persist() ?? Promise.resolve<void>(void 0))
   const embed = yield* Effect.tryPromise(getPipeline)
   return yield* Effect.tryPromise(() =>
     embed(text, {
