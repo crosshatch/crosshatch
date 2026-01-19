ed9e4c5a534ae1b17fc56feaedf524d439b139f4
Harry Solovay <harrysolovay@gmail.com>
2026-01-19 13:34:18 -0500

    clean up effect utils

--- @crosshatch/util/unwrapping.ts
+++ @crosshatch/util/unwrapping.ts
@@ -1,7 +1,10 @@
-import { Cause, Effect, Types } from "effect"
+import { Cause, Effect, Struct, Types } from "effect"
 
 export const e0 = <A, E, R>(x: Effect.Effect<Array<A>, E, R>): Effect.Effect<A | undefined, E, R> =>
   Effect.map(x, ([v]) => v)
+
+export const access = <A, K extends keyof A>(key: K) => <E, R>(x: Effect.Effect<A, E, R>) =>
+  Effect.map(x, Struct.get(key))
 
 export const slice =
   <N extends number>(n: N) =>
@@ -18,10 +21,3 @@ export const nullable = <A, E, R>(x: Effect.Effect<A, E, R>) =>
 export const nullableError =
   <C>(c: new() => C) => <A, E, R>(x: Effect.Effect<A, E | Cause.NoSuchElementException, R>) =>
     x.pipe(Effect.catchTag("NoSuchElementException", () => Effect.fail(new c())))
-
-// TODO: tuple-compatible version
-export const unwrapSlice = <N extends number>(n: N) => <A, E, R>(x: Effect.Effect<Array<A>, E, R>) =>
-  Effect.flatMap(
-    x,
-    (v) => v.length < n ? new Cause.NoSuchElementException() : Effect.succeed(v.slice(0, n) as Types.TupleOf<N, A>),
-  )
