309216946667194bf51df0cb076d1d79deda93ef
Harry Solovay <harrysolovay@gmail.com>
2026-01-21 22:26:07 -0500

    add ext

--- @crosshatch/ui/components/section.tsx
+++ @crosshatch/ui/components/section.tsx
@@ -1,4 +1,4 @@
-import { cn } from "../cn"
+import { cn } from "../cn.ts"
 
 export const Section = ({ className, ...rest }: React.ComponentProps<"div">) => (
   <div className={cn("flex w-full", className)} {...rest} />
