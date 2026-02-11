dc322fa3192c8b1445698a54005c59b21f7a8547
Harry Solovay <harrysolovay@gmail.com>
2026-02-10 21:20:36 -0500

    remove old docs

--- crosshatch/env.ts
+++ crosshatch/env.ts
@@ -4,13 +4,7 @@ export const dev = Config.boolean("DEV").pipe(
   Config.withDefault(true),
   Effect.runSync,
 )
-const prefix = dev ? "local." : ""
 
-export const domain = `${prefix}crosshatch.dev`
+export const domain = `${dev ? "local." : ""}crosshatch.dev`
 
-export const docsDomain = `${prefix}docs.crosshatch.dev`
-
-const urlLeading = (v: string) => `https://${v}`
-
-export const appUrl = urlLeading(domain)
-export const docsUrl = urlLeading(docsDomain)
+export const appUrl = `https://${domain}`
