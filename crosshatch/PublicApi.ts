9f2344dd262d8d1424dffc4b450b3eac3c911868
Harry Solovay <harrysolovay@gmail.com>
2026-02-19 14:16:30 -0500

    fix incorrect import of facilitator api group

--- crosshatch/PublicApi.ts
+++ crosshatch/PublicApi.ts
@@ -1,8 +1,9 @@
 import { HttpApi as HttpApi_, HttpApiError, OpenApi } from "@effect/platform"
-import { X402 } from "crosshatch"
+
+import { Facilitator } from "./X402/Facilitator/Facilitator.ts"
 
 export class PublicApi extends HttpApi_.make("v1")
-  .add(X402.Facilitator.Facilitator)
+  .add(Facilitator)
   .prefix("/v1")
   .addError(HttpApiError.NotFound)
   .annotate(OpenApi.Title, "crosshatch.dev")
