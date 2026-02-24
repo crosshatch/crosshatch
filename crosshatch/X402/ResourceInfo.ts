7b5cbf2ba7718cb55668c0009ad7cfbf7569bffc
Harry Solovay <harrysolovay@gmail.com>
2026-02-24 08:09:38 -0500

    make resource mime type optional

--- crosshatch/X402/ResourceInfo.ts
+++ crosshatch/X402/ResourceInfo.ts
@@ -2,6 +2,6 @@ import { Schema as S } from "effect"
 
 export const ResourceInfo = S.Struct({
   description: S.String,
-  mimeType: S.String,
   url: S.String,
+  mimeType: S.String.pipe(S.optional),
 })
