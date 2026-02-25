3fdbc4fa5467eee14d6cca7defe224cbdb202dd0
Harry Solovay <harrysolovay@gmail.com>
2026-02-25 09:12:14 -0500

    add derivation util in crosshatch x402 util

--- @crosshatch/util/widget/popup.ts
+++ @crosshatch/util/widget/popup.ts
@@ -1,10 +1,8 @@
-// TODO: refactor
 import { Effect, Schema as S, Stream } from "effect"
 
 import type { WidgetConfig } from "./self.ts"
 
-import * as ParentContext from "../ParentContext.ts"
-
+// TODO: revamp
 export const popup = <A, I>({ src, item = S.Never as never }: WidgetConfig<A, I>) =>
   Stream.asyncScoped<A>((emit) => {
     const { origin: expectedOrigin } = new URL(src)
@@ -22,9 +20,6 @@ export const popup = <A, I>({ src, item = S.Never as never }: WidgetConfig<A, I>
       "message",
       async ({ data, origin }) => {
         if (origin === expectedOrigin) {
-          if (S.is(ParentContext.RequestIntroduction)(data)) {
-            context?.postMessage(ParentContext.Introduction.make(), origin)
-          }
           const option = decodeOption(data)
           if (option._tag === "Some") {
             const { value } = option
--- crosshatch/X402/Derivation.ts
+++ crosshatch/X402/Derivation.ts
@@ -1,4 +1,5 @@
-import { Schema as S } from "effect"
+import { Schema as S, Redacted } from "effect"
+import { Address as OxAddress, HdKey, Mnemonic, Secp256k1 } from "ox"
 
 import { Address } from "./Address.ts"
 import { ChainId } from "./ChainId.ts"
@@ -9,3 +10,18 @@ export const Derivation = S.Struct({
   chainId: ChainId,
   namespace: Namespace,
 })
+
+export const deriveCommon = (mnemonic: Redacted.Redacted<string>): Array<typeof Derivation.Type> => {
+  const seed = Mnemonic.toSeed(Redacted.value(mnemonic).trim())
+  const root = HdKey.fromSeed(seed)
+  const { privateKey } = root.derive("m/44'/60'/0'/0/0")
+  const publicKey = Secp256k1.getPublicKey({ privateKey })
+  const address = OxAddress.fromPublicKey(publicKey)
+  return [
+    {
+      address,
+      chainId: "8453",
+      namespace: "eip155",
+    },
+  ]
+}
