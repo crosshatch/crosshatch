8ac0e118b1d50aafb60527e156da14128bebe0c2
Harry Solovay <harrysolovay@gmail.com>
2026-02-24 21:52:57 -0500

    add ArrayBuffer schema for use in webauthn schemas

--- @crosshatch/util/schema.ts
+++ @crosshatch/util/schema.ts
@@ -7,3 +7,13 @@ export const UnknownRecord = S.Record({
   key: S.String,
   value: S.Unknown,
 })
+
+export const ArrayBuffer_ = S.transform(S.Uint8ArrayFromBase64, S.instanceOf(ArrayBuffer), {
+  strict: true,
+  encode: (ab) => new Uint8Array(ab),
+  decode: (u8a) => {
+    const a = new ArrayBuffer(u8a.byteLength)
+    new Uint8Array(a).set(u8a)
+    return a
+  },
+})
