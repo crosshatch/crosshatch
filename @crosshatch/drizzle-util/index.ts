41dcaf42d47bf92f87d3ebda07193bf2abf29c9b
Harry Solovay <harrysolovay@gmail.com>
2026-03-07 17:49:57 -0500

    refactoring env and remove old drizzle util copy

--- @crosshatch/drizzle-util/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./standard.ts"
-export * from "./custom_types.ts"
-export * from "./uniqueIndices.ts"
-export * from "./columns.ts"
--- crosshatch/CrosshatchEnv.ts
+++ crosshatch/CrosshatchEnv.ts
@@ -2,26 +2,24 @@ import { Config, Context, Effect, Layer } from "effect"
 
 import { tag } from "./tag.ts"
 
-export class CrosshatchEnv extends Context.Tag(tag("CrosshatchEnv"))<
-  CrosshatchEnv,
-  {
+export declare namespace CrosshatchEnv {
+  export type Service = {
     readonly dev: boolean
     readonly domain: string
     readonly url: string
-    readonly isCrosshatch: (origin: string) => boolean
-    readonly href: (subpath: string) => string
   }
->() {
-  static readonly layer = Effect.gen(function* () {
-    const dev = yield* Config.boolean("DEV").pipe(Config.withDefault(true))
-    const domain = `${dev ? "local." : ""}x.crosshatch.dev`
-    const url = `https://${domain}`
-    return {
-      dev,
-      domain,
-      href: (subpath: string) => `${url}/${subpath}`,
-      isCrosshatch: (origin: string) => origin === url,
-      url,
-    } as const
-  }).pipe(Layer.effect(CrosshatchEnv))
 }
+
+export class CrosshatchEnv extends Context.Tag(tag("CrosshatchEnv"))<CrosshatchEnv, CrosshatchEnv.Service>() {}
+
+// TODO: move to internal util
+export const isCrosshatch = (origin: string) => CrosshatchEnv.pipe(Effect.map(({ url }) => origin === url))
+
+export const layer = Effect.gen(function* () {
+  const dev = yield* Config.boolean("DEV").pipe(Config.withDefault(true))
+  const domain = `${dev ? "local." : ""}x.crosshatch.dev`
+  const url = `https://${domain}`
+  return { dev, domain, url } as const
+}).pipe(Layer.effect(CrosshatchEnv))
+
+export const href = (subpath: string) => CrosshatchEnv.pipe(Effect.map(({ url }) => `${url}/${subpath}`))
