aba63fb5dcd0ae6d62f43983603960a39e470178
Harry Solovay <harrysolovay@gmail.com>
2026-05-29 17:39:41 -0400

    continued

--- crosshatch/InternalEnv.ts
+++ crosshatch/InternalEnv.ts
@@ -24,7 +24,7 @@ export class InternalEnv extends Context.Service<
 }) {
   static readonly layer = Layer.effect(this, this.make)
 
-  static readonly href = (subpath: string) => this.asEffect().pipe(Effect.map(({ url }) => `${url}/${subpath}`))
+  static readonly href = (subpath: string) => this.pipe(Effect.map(({ url }) => `${url}/${subpath}`))
 
   static readonly isCrosshatch = (origin: string) =>
     origin === CROSSHATCH_URL || origin === `${CROSSHATCH_URL}.localhost`
