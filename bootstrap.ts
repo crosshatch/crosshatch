a5fbcf2812c4956b3643c83080e366ceac50a892
Harry Solovay <harrysolovay@gmail.com>
2026-06-06 16:14:00 -0400

    misc cleanup

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,14 +1,19 @@
 import * as Alchemy from "alchemy"
 import * as Cloudflare from "alchemy/Cloudflare"
 import * as Github from "alchemy/GitHub"
-import { Layer } from "effect"
-import { bootstrap } from "liminal-util/alchemicals/bootstrap"
+import { Layer, Effect } from "effect"
+import { GithubDeployer } from "liminal-util/alchemicals/GithubDeployer"
 
 export default Alchemy.Stack(
-  "crosshatch-github",
+  "github-crosshatch-crosshatch",
   {
     state: Cloudflare.state(),
     providers: Layer.mergeAll(Github.providers(), Cloudflare.providers()),
   },
-  bootstrap({ repository: "crosshatch" }),
+  Effect.gen(function* () {
+    yield* GithubDeployer({
+      owner: "crosshatch",
+      repository: "crosshatch",
+    })
+  }),
 )
