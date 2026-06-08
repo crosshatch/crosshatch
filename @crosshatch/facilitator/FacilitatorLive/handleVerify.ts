643fdcffb0ba625aa0f971882e4ccf2ad9f427ca
Harry Solovay <harrysolovay@gmail.com>
2026-06-08 18:13:30 -0400

    misc.

--- @crosshatch/facilitator/FacilitatorLive/handleVerify.ts
+++ @crosshatch/facilitator/FacilitatorLive/handleVerify.ts
@@ -2,12 +2,23 @@ import { Effect } from "effect"
 
 import { handler } from "../_httpapi_util.ts"
 import { Api } from "../Api.ts"
-import { verify } from "../verify.ts"
+import { make } from "../CdpClient.ts"
 
 export const handleVerify = handler(
   Api,
   "facilitator",
   "verify",
-  ({ payload: { paymentPayload: payload, paymentRequirements: requirements } }) =>
-    verify({ payload, requirements }).pipe(Effect.orDie),
+  Effect.fn(function* ({ payload: { paymentPayload, paymentRequirements } }) {
+    const client = yield* make({
+      host: "api.cdp.coinbase.com",
+      path: "/platform/v2/x402/verify",
+    })
+    return yield* client.verifyX402Payment({
+      payload: {
+        x402Version: 2,
+        paymentRequirements,
+        paymentPayload,
+      } as never, // TODO
+    })
+  }, Effect.orDie),
 )
