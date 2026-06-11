f8c71e24a4ce837018962caac27479a935c0e0fb
Harry Solovay <harrysolovay@gmail.com>
2026-06-11 15:08:49 -0400

    debugging facilitator deployment

--- @crosshatch/facilitator/alchemy.run.ts
+++ @crosshatch/facilitator/alchemy.run.ts
@@ -1,21 +1,23 @@
 import * as Alchemy from "alchemy"
 import * as Cloudflare from "alchemy/Cloudflare"
+import * as Github from "alchemy/GitHub"
 import { Effect, Config, Layer } from "effect"
 import * as AlchemicalEnv from "liminal-util/alchemicals/AlchemicalEnv"
+import { PrPreviewComment } from "liminal-util/alchemicals/PrComment"
 import { WorkerConfig } from "liminal-util/alchemicals/WorkerConfig"
 
 export default Alchemy.Stack(
   "crosshatch-facilitator",
   {
     state: Cloudflare.state(),
-    providers: Layer.mergeAll(Cloudflare.providers()),
+    providers: Layer.mergeAll(Cloudflare.providers(), Github.providers()),
   },
   Effect.gen(function* () {
     const base = yield* WorkerConfig({
       domain: "facilitator.crosshatch.dev",
     })
     const { dev: DEV } = yield* Alchemy.AlchemyContext
-    return yield* Cloudflare.Worker("Entry", {
+    const { url } = yield* Cloudflare.Worker("Entry", {
       ...base,
       main: "main.ts",
       env: {
@@ -24,5 +26,6 @@ export default Alchemy.Stack(
         CDP_API_KEY_SECRET: Config.redacted("CDP_API_KEY_SECRET"),
       },
     })
+    yield* PrPreviewComment({ name: "Facilitator", url })
   }).pipe(Effect.provide(AlchemicalEnv.layer)),
 )
