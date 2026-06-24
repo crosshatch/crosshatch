7626ac44b2b164bde57e39c65a439ec46f48620c
Harry Solovay <harrysolovay@gmail.com>
2026-06-23 21:17:22 -0400

    more on asset interop story

--- crosshatch/Evm/Permit2Payload.ts
+++ crosshatch/Evm/Permit2Payload.ts
@@ -47,10 +47,7 @@ const permit2WitnessTypes = {
   ],
 } as const
 
-export const make = Effect.fn(function* (
-  signer: EvmSigner["Service"],
-  requirement: typeof Requirements.Requirements.Type,
-) {
+export const make = Effect.fn(function* (signer: EvmSigner, requirement: typeof Requirements.Requirements.Type) {
   const now = Math.floor(Date.now() / 1000)
   const chainId = parseInt(requirement.network.split(":")[1]!)
   const nonce = BigInt(toHex(crypto.getRandomValues(new Uint8Array(32)))).toString()
