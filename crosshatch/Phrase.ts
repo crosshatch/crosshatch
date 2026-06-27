10701e20eafcc2f549002a299521e467785749b3
Harry Solovay <harrysolovay@gmail.com>
2026-06-27 18:31:59 -0400

    phrase config

--- crosshatch/Phrase.ts
+++ crosshatch/Phrase.ts
@@ -1,9 +1,13 @@
-import { Redacted, Effect, Schema as S } from "effect"
+import { flow, Redacted, Effect, Schema as S, Config } from "effect"
 import { Mnemonic } from "ox"
 
-export const Phrase = S.String.pipe(S.brand("Phrase"))
+export const Phrase = S.String.check(
+  S.isPattern(/^(?:(?:[a-z]+ ){11}|(?:[a-z]+ ){14}|(?:[a-z]+ ){17}|(?:[a-z]+ ){20}|(?:[a-z]+ ){23})[a-z]+$/),
+).pipe(S.brand("Phrase"))
 
 export const PhraseRedacted = S.Redacted(Phrase)
+
+export const config = flow(Config.string, Config.map(flow(Phrase.make, Redacted.make)))
 
 export const fromText = (text: string) => Redacted.make(Phrase.make(text))
 
