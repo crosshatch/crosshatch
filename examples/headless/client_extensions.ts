3bf998e66439d0eb261aec058abce3757c7a75af
Harry Solovay <harrysolovay@gmail.com>
2026-07-05 20:47:03 -0400

    update docs, clean up examples

--- examples/headless/client_extensions.ts
+++ examples/headless/client_extensions.ts
@@ -1,9 +1,19 @@
-import { KnownAssets, Facilitator, Required, Requirements, Payload } from "crosshatch"
+import { KnownAssets, Facilitator, Required, Requirements, Payload, Extension, PaymentId } from "crosshatch"
 import { EvmAddress } from "crosshatch/Evm"
-import { Effect } from "effect"
+import { Effect, Layer } from "effect"
 import { FetchHttpClient } from "effect/unstable/http"
 
 import { PayerLive } from "./_common.ts"
+
+const PaymentIdHandlerLive = Extension.layerHandler(
+  PaymentId.PaymentIdExtension,
+  Effect.fn(function* ({ required }) {
+    return {
+      required,
+      id: PaymentId.PaymentId.make(crypto.randomUUID()),
+    }
+  }),
+)
 
 Effect.gen(function* () {
   const EVM_ADDRESS = yield* EvmAddress.config("PAY_TO_EVM")
@@ -12,6 +22,9 @@ Effect.gen(function* () {
   | Description of the charge.
   |
   `.pipe(
+    Required.extend(PaymentId.PaymentIdExtension, {
+      required: true,
+    }),
     Required.accept(
       Requirements.group(KnownAssets.USDC, {
         amount: 0.01,
@@ -22,4 +35,4 @@ Effect.gen(function* () {
   )
   const { payload } = yield* Payload.make({ required })
   yield* Facilitator.settle({ payload })
-}).pipe(Effect.provide([FetchHttpClient.layer, PayerLive]), Effect.runFork)
+}).pipe(Effect.provide([FetchHttpClient.layer, PayerLive.pipe(Layer.provide(PaymentIdHandlerLive))]), Effect.runFork)
