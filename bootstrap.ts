cf2a04b0bd757c95ce0dc24f95d841bbbb484278
Harry Solovay <harrysolovay@gmail.com>
2026-07-10 15:05:19 -0400

    continued

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,7 +1,7 @@
 import * as Alchemy from "alchemy"
 import * as Cloudflare from "alchemy/Cloudflare"
 import * as Github from "alchemy/GitHub"
-import { EvmAddress } from "crosshatch/Evm"
+import { Eip155Address } from "crosshatch/Eip155"
 import { Layer, Effect, Config } from "effect"
 import { GithubDeployer } from "liminal-util/alchemicals/GithubDeployer"
 
@@ -21,7 +21,7 @@ export default Alchemy.Stack(
       repository,
       variables: {
         CDP_API_KEY_ID: Config.string("CDP_API_KEY_ID"),
-        PAY_TO_EVM: EvmAddress.config("PAY_TO_EVM"),
+        PAY_TO_EIP155: Config.schema(Eip155Address.Eip155Address, "PAY_TO_EIP155"),
         OTEL_EXPORTER_OTLP_ENDPOINT: "https://ingest.us2.signoz.cloud",
       },
     })
