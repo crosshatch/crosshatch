e3fd17481bdc18eb8e37c5803ad342bf897224c5
Harry Solovay <harrysolovay@gmail.com>
2026-07-11 14:49:24 -0400

    update typescript

--- crosshatch/Mnemonic.ts
+++ crosshatch/Mnemonic.ts
@@ -12,16 +12,21 @@ type Mnemonic_ = [Redacted.Redacted<string & Brand.Brand<typeof ID>>][0]
 export interface Mnemonic extends S.Redacted<S.brand<S.String, typeof ID>>, Context.Service<Mnemonic, Mnemonic_> {
   new (_: never): Context.ServiceClass.Shape<typeof ID, Mnemonic_>
 }
+
 export const Mnemonic: Mnemonic = Object.assign(Context.Service<Mnemonic, Mnemonic_>()(ID), S.Redacted(MnemonicText))
 
 export const fromText = (text: string) => Redacted.make(MnemonicText.make(text))
+
 export const layerText = flow(fromText, Layer.succeed(Mnemonic))
 
 export const fromConfig = (config: Config.Config<string>) => Config.map(config, fromText)
+
 export const layerConfig = flow(fromConfig, Layer.effect(Mnemonic))
 
 export const env = fromConfig(Config.string("MNEMONIC"))
+
 export const layerEnv = Layer.effect(Mnemonic, env)
 
 export const random = Effect.sync(() => fromText(OxMnemonic.random(OxMnemonic.english)))
+
 export const layerRandom = Layer.effect(Mnemonic, random)
