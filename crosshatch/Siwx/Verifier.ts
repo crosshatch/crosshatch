6dc81ca4d02d2fc471ab89981378761b30577d9d
Sai Ashirwad <saiashirwadr@gmail.com>
2026-07-21 03:48:16 +0530

    remove Verifier.Error

--- crosshatch/Siwx/Verifier.ts
+++ crosshatch/Siwx/Verifier.ts
@@ -12,6 +12,5 @@ export interface Verifier<E = unknown, R = never> {
 
 export declare namespace Verifier {
   export type Any = Verifier<any, any>
-  export type Error<T extends Any> = T extends Verifier<infer E, any> ? E : never
   export type Context<T extends Any> = T extends Verifier<any, infer R> ? R : never
 }
