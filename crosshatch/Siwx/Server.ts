19b0091587908a09c169f67eec8aeaf40a8fa4eb
Sai Ashirwad <saiashirwadr@gmail.com>
2026-07-21 06:06:05 +0530

    clean up Siwx.Server

--- crosshatch/Siwx/Server.ts
+++ crosshatch/Siwx/Server.ts
@@ -1,8 +1,7 @@
-import { Context, Effect, Option, pipe, Schema as S } from "effect"
+import { Effect, Option, pipe, Schema as S } from "effect"
 import { Headers, HttpRouter, HttpServerRequest } from "effect/unstable/http"
 
 import { RequiredUrl } from "../Required.ts"
-import { SiwxError } from "./Error.ts"
 import { Identity } from "./Identity.ts"
 import { ProofFromBase64JsonString, SIGN_IN_WITH_X } from "./Schema.ts"
 import * as Verification from "./Verification.ts"
@@ -20,34 +19,26 @@ export const layerMiddleware = <const Verifiers extends ReadonlyArray<Verifier.A
       return (effect) =>
         Effect.gen(function* () {
           const request = yield* HttpServerRequest.HttpServerRequest
-          const requestUrl = HttpServerRequest.toURL(request).pipe(
-            Option.map((incoming) => new URL(`${origin}${incoming.pathname}${incoming.search}`)),
-          )
+          const requestUrl = Option.liftThrowable((url: string) => new URL(url, origin))(request.url)
+
           const identity = yield* pipe(
             Option.all({ encodedProof: Headers.get(SIGN_IN_WITH_X)(request.headers), requestUrl }),
             Option.map(({ encodedProof, requestUrl }) =>
-              Effect.gen(function* () {
-                const proof = yield* S.decodeUnknownEffect(ProofFromBase64JsonString)(encodedProof).pipe(
-                  Effect.mapError((cause) => new SiwxError({ cause })),
-                )
-                return yield* Verification.verifyProof(...verifiers)(proof, requestUrl)
-              }),
+              S.decodeUnknownEffect(ProofFromBase64JsonString)(encodedProof).pipe(
+                Effect.flatMap((proof) => Verification.verifyProof(...verifiers)(proof, requestUrl)),
+              ),
             ),
             Effect.transposeOption,
             Effect.map(Option.getOrUndefined),
-            Effect.catchTag("SiwxError", Effect.as(undefined)),
+            Effect.catchTag("SiwxError", () => Effect.succeed(undefined)),
           )
-
-          return yield* Effect.provideContext(
-            effect,
-            pipe(
-              Context.make(Identity, identity),
-              Context.add(
-                RequiredUrl,
-                requestUrl.pipe(
-                  Option.map(({ href }) => href),
-                  Option.getOrUndefined,
-                ),
+          return yield* effect.pipe(
+            Effect.provideService(Identity, identity),
+            Effect.provideService(
+              RequiredUrl,
+              requestUrl.pipe(
+                Option.map(({ href }) => href),
+                Option.getOrUndefined,
               ),
             ),
           )
