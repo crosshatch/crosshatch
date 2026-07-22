0f840110ec46499ed66585785012c09e98473356
Sai Ashirwad <saiashirwadr@gmail.com>
2026-07-22 23:42:53 +0530

    cleanup SolanaState

--- crosshatch/Solana/Protocol/Rpc.ts
+++ crosshatch/Solana/Protocol/Rpc.ts
@@ -1,8 +1,7 @@
 import { Effect, Schema as S } from "effect"
-import { HttpClient, HttpClientRequest, HttpClientResponse } from "effect/unstable/http"
+import { FetchHttpClient, HttpClient, HttpClientRequest, HttpClientResponse } from "effect/unstable/http"
 
 import { Blockhash } from "./Address.ts"
-import type { LifetimeConstraint } from "./TransactionMessage.ts"
 
 const GetLatestBlockhashResponse = S.Struct({
   body: S.Struct({
@@ -15,18 +14,22 @@ const GetLatestBlockhashResponse = S.Struct({
   }),
 })
 
-export const getLatestBlockhash = HttpClientRequest.post("/").pipe(
-  HttpClientRequest.bodyJsonUnsafe({ jsonrpc: "2.0", id: 1, method: "getLatestBlockhash" }),
-  HttpClient.execute,
-  Effect.flatMap(HttpClientResponse.schemaJson(GetLatestBlockhashResponse)),
-  Effect.map(
-    ({
-      body: {
-        result: { value },
-      },
-    }): LifetimeConstraint => ({
-      blockhash: value.blockhash,
-      lastValidBlockHeight: BigInt(value.lastValidBlockHeight),
-    }),
-  ),
-)
+export const getLatestBlockhash = (url: string) =>
+  HttpClientRequest.post(url).pipe(
+    HttpClientRequest.bodyJsonUnsafe({ jsonrpc: "2.0", id: 1, method: "getLatestBlockhash" }),
+    HttpClient.execute,
+    Effect.flatMap(HttpClientResponse.schemaJson(GetLatestBlockhashResponse)),
+    Effect.map(
+      ({
+        body: {
+          result: {
+            value: { blockhash, lastValidBlockHeight },
+          },
+        },
+      }) => ({
+        blockhash,
+        lastValidBlockHeight: BigInt(lastValidBlockHeight),
+      }),
+    ),
+    Effect.provide(FetchHttpClient.layer),
+  )
--- crosshatch/Solana/SolanaState.ts
+++ crosshatch/Solana/SolanaState.ts
@@ -1,5 +1,4 @@
 import { Effect, Layer } from "effect"
-import { FetchHttpClient, HttpClient, HttpClientRequest } from "effect/unstable/http"
 
 import * as State from "../State.ts"
 import { Blockhash, getLatestBlockhash } from "./Protocol/Protocol.ts"
@@ -7,15 +6,8 @@ import { Blockhash, getLatestBlockhash } from "./Protocol/Protocol.ts"
 export class SolanaState extends State.Service<SolanaState, Blockhash>()("crosshatch/Solana/SolanaState") {}
 
 export const layer = (url: string) =>
-  Layer.effect(
-    SolanaState,
-    HttpClient.HttpClient.pipe(
-      Effect.map((client) => HttpClient.mapRequest(client, HttpClientRequest.prependUrl(url))),
-      Effect.map((client) => ({
-        getLatestBlockhash: getLatestBlockhash.pipe(
-          Effect.provideService(HttpClient.HttpClient, client),
-          Effect.mapError((cause) => new State.GetLatestBlockhashError({ cause })),
-        ),
-      })),
+  Layer.succeed(SolanaState, {
+    getLatestBlockhash: getLatestBlockhash(url).pipe(
+      Effect.mapError(({ cause }) => new State.GetLatestBlockhashError({ cause })),
     ),
-  ).pipe(Layer.provide(FetchHttpClient.layer))
+  })
