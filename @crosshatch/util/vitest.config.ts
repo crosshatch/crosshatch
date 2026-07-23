f1ffcdae47b2ad487a8b3a0c4d4522c2022c8f73
Harry Solovay <harrysolovay@gmail.com>
2026-07-23 14:26:58 -0400

    vitest cleanup

--- @crosshatch/util/vitest.config.ts
+++ @crosshatch/util/vitest.config.ts
@@ -1,6 +1,6 @@
 import { mergeConfig, type ViteUserConfig } from "vitest/config"
 
-import config from "../konfik/vitest.ts"
+import config from "../../konfik/vitest.ts"
 
 export default mergeConfig(config, {
   test: { name: "@crosshatch/util" },
