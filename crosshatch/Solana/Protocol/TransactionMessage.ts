14cc86fdf44837394f4e49efb9847378cc1e92a7
Sai Ashirwad <saiashirwadr@gmail.com>
2026-07-24 18:29:34 +0530

    cleanup

--- crosshatch/Solana/Protocol/TransactionMessage.ts
+++ crosshatch/Solana/Protocol/TransactionMessage.ts
@@ -1,34 +1,24 @@
 import type { Address, Blockhash } from "./Address.ts"
 
-export const AccountRole = {
-  READONLY: 0,
-  WRITABLE: 1,
-  READONLY_SIGNER: 2,
-  WRITABLE_SIGNER: 3,
-} as const
-
+export const AccountRole = { READONLY: 0, WRITABLE: 1, READONLY_SIGNER: 2, WRITABLE_SIGNER: 3 }
 export type AccountRole = (typeof AccountRole)[keyof typeof AccountRole]
-
-interface AccountMeta {
-  readonly address: Address
-  readonly role: AccountRole
-}
 
 export interface Instruction {
   readonly programAddress: Address
-  readonly accounts?: ReadonlyArray<AccountMeta>
+  readonly accounts?: ReadonlyArray<{
+    readonly address: Address
+    readonly role: AccountRole
+  }>
   readonly data?: Uint8Array
-}
-
-interface LifetimeConstraint {
-  readonly blockhash: Blockhash
-  readonly lastValidBlockHeight: bigint
 }
 
 export interface TransactionMessage {
   readonly version: 0
   readonly feePayer: Address
-  readonly lifetimeConstraint: LifetimeConstraint
+  readonly lifetimeConstraint: {
+    readonly blockhash: Blockhash
+    readonly lastValidBlockHeight: bigint
+  }
   readonly instructions: ReadonlyArray<Instruction>
 }
 
