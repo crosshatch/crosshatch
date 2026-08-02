8b2d774222dc390ccb09615b442c792e7d6dd6e3
Harry Solovay <harrysolovay@gmail.com>
2026-08-02 09:04:20 -0400

    cleanup

--- examples/headless/Prelude.ts
+++ examples/headless/Prelude.ts
@@ -1,6 +1,4 @@
-import { NodeServices } from "@effect/platform-node"
-import { Accept, Payer, MnemonicStore } from "crosshatch"
-import * as ChxNodeServices from "crosshatch/ChxNodeServices"
+import { Accept, Payer, Mnemonic } from "crosshatch"
 import * as Known from "crosshatch/Known"
 import { UnifiedSchemes } from "crosshatch/Unified"
 import { Config, Layer } from "effect"
@@ -8,6 +6,8 @@ import { Config, Layer } from "effect"
 export const layer = Payer.layerLocal({
   accept: Accept.first(Known),
   schemes: UnifiedSchemes.layer({
-    solana: { rpc: Config.string("SOLANA_RPC_URL").pipe(Config.withDefault(undefined)) },
-  }).pipe(Layer.provide(MnemonicStore.layerMnemonicFromName().pipe(Layer.provide(ChxNodeServices.layer)))),
-}).pipe(Layer.provideMerge(NodeServices.layer))
+    solana: {
+      rpc: Config.string("SOLANA_RPC_URL").pipe(Config.withDefault(undefined)),
+    },
+  }).pipe(Layer.provide(Mnemonic.layerFromEnv)),
+})
