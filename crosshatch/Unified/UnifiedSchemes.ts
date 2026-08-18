38913283aa7d4b8600d589f300e3594c78d83038
Harry Solovay <harrysolovay@gmail.com>
2026-08-17 22:21:27 -0400

    continued

--- crosshatch/Unified/UnifiedSchemes.ts
+++ crosshatch/Unified/UnifiedSchemes.ts
@@ -1,9 +1,20 @@
-import { Layer } from "effect"
+import { Effect, Layer, Option } from "effect"
 
 import { Erc3009Scheme, Eip155Signer, Permit2Scheme } from "../Eip155/index.ts"
-import { SolanaScheme, SolanaSigner } from "../Solana/index.ts"
+import { SolanaScheme, SolanaSigner, SolanaState } from "../Solana/index.ts"
 
 export const layer = Layer.mergeAll(
   Layer.mergeAll(Erc3009Scheme.layer, Permit2Scheme.layer).pipe(Layer.provide(Eip155Signer.layerFromMnemonic)),
-  SolanaScheme.layer.pipe(Layer.provide(SolanaSigner.layerFromMnemonic)),
+  Effect.serviceOption(SolanaState.SolanaState).pipe(
+    Effect.map(
+      Option.match({
+        onSome: (v) =>
+          SolanaScheme.layer.pipe(
+            Layer.provide([SolanaSigner.layerFromMnemonic, Layer.succeed(SolanaState.SolanaState, v)]),
+          ),
+        onNone: () => Layer.empty,
+      }),
+    ),
+    Layer.unwrap,
+  ),
 )
