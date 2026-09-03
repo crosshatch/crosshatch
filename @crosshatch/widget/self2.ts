cf49264a1d62efb11631dbcf08ed0be6387bd55d
Harry Solovay <4893548+harrysolovay@users.noreply.github.com>
2026-09-03 13:23:54 -0400

    blah

---
+++ @crosshatch/widget/self2.ts
@@ -0,0 +1,15 @@
+import { Schema as S, Struct } from "effect"
+
+export const parent = () => (globalThis.opener as Window | null) ?? globalThis.parent
+
+const ChxWidgetEventTypeId = "~@crosshatch/widget/Self/ChxWidgetEvent" as const
+
+const fields = Struct.assign({
+  [ChxWidgetEventTypeId]: S.tag(ChxWidgetEventTypeId),
+})
+
+export const ChxWidgetEvent = S.TaggedUnion({
+  Done: fields({}),
+})
+
+export const postFinished = () => parent().postMessage(ChxWidgetEvent.cases.Done.make({}), "*")
